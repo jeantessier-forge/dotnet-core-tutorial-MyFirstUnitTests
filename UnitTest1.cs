d5f888f2fbbdee0de6c6465199f329dd3a2fbd2a
Jean Tessier <jean.tessier@forgeglobal.com>
2022-01-27 18:18:52 -0800

    Add some tests
    The theory passes in expected values so tests can all pass.

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -7,6 +7,31 @@ public class UnitTest1
     [Fact]
     public void Test1()
     {
+        Assert.Equal(4, Add(2, 2));
+    }
 
+    [Fact]
+    public void FailingTest()
+    {
+        Assert.Equal(5, Add(2, 2));
+    }
+
+    int Add(int x, int y)
+    {
+        return x + y;
+    }
+
+    [Theory]
+    [InlineData(3, true)]
+    [InlineData(5, true)]
+    [InlineData(6, false)]
+    public void MyFirstTheory(int value, bool expectedResult)
+    {
+        Assert.Equal(expectedResult, IsOdd(value));
+    }
+
+    bool IsOdd(int value)
+    {
+        return value % 2 == 1;
     }
 }
